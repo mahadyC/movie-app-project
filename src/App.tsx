297bf77b1f0b3fe8797d7a06fcae14b4c4a6393d
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-11 19:37:14 +0200

    Render popular movie titles from the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import './App.css';
 import * as api from './api';
 class App extends Component {
 
+  state = {
+    popular_movies: []
+  }
   componentDidMount() {
     api.getPopularMovies().then((movieList) => {
       console.log(movieList);
+      this.setState({popular_movies: movieList})
     })
   }
   render() {
+    console.log(this.state.popular_movies)
     return (
-      <div className="App">       
+      <div className="App">   
+        <ul>
+          {this.state.popular_movies.map((movie:any) => 
+            <li key={movie.id}>{movie.title}</li>
+            )}  
+        </ul>    
       </div>
     );
   }
