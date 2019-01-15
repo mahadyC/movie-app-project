7b5946c180a87e30cad0a4d2b92d96b500cc4bf2
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-15 21:01:03 +0200

    Render popular movies like design

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as api from './api';
+import { url } from 'inspector';
+
+type Movie = {
+  id: string;
+  title: string;
+  release_date: string;
+  poster_path: string;
+  vote_average: number;
+}
+
+type State = {
+  popular_movies: Movie[]
+}
+
+const containerStyle = {
+  display: 'grid',
+  justifyContent: 'space-evenly',
+  gridTemplateColumns: 'auto auto auto auto ',
+  gridGap: 10,
+  backgroundColor: '#113049',
+  padding: 10,
+  overflow: 'hidden'
+}
+
 class App extends Component {
 
-  state = {
+  state: State = {
     popular_movies: []
   }
   componentDidMount() {
@@ -16,12 +40,16 @@ class App extends Component {
   render() {
     console.log(this.state.popular_movies)
     return (
-      <div className="App">   
-        <ul>
-          {this.state.popular_movies.map((movie:any) =>          
-            <li key={movie.id}>{movie.title}</li>                  
+      <div className="App">  
+        <div className='grid-container' style={containerStyle}>
+          {this.state.popular_movies.map((movie) =>          
+            <div key={movie.id} className='grid-item' style={{textAlign: "center", padding: 30, fontSize: 30, position: "relative"}}>
+              <div className='movie-card-top' style={{width: 342, height: 500, backgroundImage: `url(https://image.tmdb.org/t/p/w342/${movie.poster_path})`, borderRadius: 5}}></div>
+              <div className='movie-release-date' style={{textAlign: "left", color: "#5A5F61", fontSize: 16, paddingTop: 20}}>{movie.release_date}</div>
+              <div className='movie-title' style={{textAlign: "left", color: "#AFBABA", fontSize: 20}}>{movie.title}</div>             
+            </div>                  
           )}  
-        </ul>    
+        </div>    
       </div>
     );
   }
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 
-const api:string = 'https://api.themoviedb.org/3/movie';
-const API_KEY:any = process.env.REACT_APP_TMDB_API_KEY;
+const api: string = 'https://api.themoviedb.org/3/movie';
+const API_KEY: string = process.env.REACT_APP_TMDB_API_KEY!;
 
 export const getPopularMovies = () =>
   fetch(`${api}/popular?api_key=${API_KEY}&language=en-US&page=1`)
