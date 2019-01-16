cfcf0a730ec315f4b78626dafe3061bcc4b1dbe1
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-16 08:54:00 +0200

    API language fix

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,6 @@ const api: string = 'https://api.themoviedb.org/3/movie';
 const API_KEY: string = process.env.REACT_APP_TMDB_API_KEY!;
 
 export const getPopularMovies = () =>
-  fetch(`${api}/popular?api_key=${API_KEY}&language=en-US&page=1`)
+  fetch(`${api}/popular?api_key=${API_KEY}&page=1`)
     .then(res => res.json())
     .then(data => data.results)
