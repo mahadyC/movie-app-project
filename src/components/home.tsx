22a5c50379876de147545a1dc81fefa4bc382af5
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-18 12:34:50 +0200

    Clean up the home component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as api from '../api';
 import '../movie.css';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 type Movie = {
     id: string;
@@ -12,20 +12,13 @@ type Movie = {
     overview: string;
 }
 
-type Genres = {
-    id: number;
-    name: string;
-}
-
 type State = {
     popular_movies: Movie[];
-    movie_genres: Genres[];
 }
 
 class Home extends Component {
     state: State = {
-        popular_movies: [],
-        movie_genres: []
+        popular_movies: []    
     }
 
     componentDidMount() {
@@ -34,17 +27,12 @@ class Home extends Component {
         })
     }
 
-    fetchMovieDetail = (movieId: string) => {
-        api.getMovieDetail(movieId)
-          .then(data => this.setState({movie_genres: data}))
-    }
-
     render() {
         return(
             <div className='grid-container'>
                 {this.state.popular_movies.map((movie) =>          
                     <div key={movie.id} className='grid-item'>
-                        <Link to={`/${movie.id}`} onClick={() => this.fetchMovieDetail(movie.id)}>
+                        <Link to={`/${movie.id}`}>
                         <div className='movie-card-top' style={{backgroundImage: `url(https://image.tmdb.org/t/p/w342/${movie.poster_path})`}}></div>
                         </Link> 
                         <div className='movie-release-date'>{movie.release_date}</div>
