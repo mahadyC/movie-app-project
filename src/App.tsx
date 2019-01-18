8ed4570d47553b3a9bf8383532a8d8e18127bc1b
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-18 18:29:16 +0200

    Move browser router from index to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, BrowserRouter} from 'react-router-dom';
 import MovieDetail from './components/movie-detail';
 import Home from './components/home';
 
@@ -8,10 +8,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">  
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/:movieId" component={MovieDetail}></Route>     
-      </div>
+      <Router>
+        <div className="App">  
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/:movieId" component={MovieDetail}></Route>     
+        </div>
+      </Router>
+      
     );
   }
 }
