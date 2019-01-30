4c170dc6a8459772e7ce2b5314a636b01ebc5950
mahadyC <mahady.chowdhury@metropolia.fi>
2019-01-30 10:50:10 +0200

    Updates

---
+++ precache-manifest.545a91646e75aa3ae7aae0daf782b392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3a6d10bc12c35e6272",
+    "url": "/movie-app-project/static/js/runtime~main.ac3a6d10.js"
+  },
+  {
+    "revision": "79fd940452eb42c66221",
+    "url": "/movie-app-project/static/js/main.79fd9404.chunk.js"
+  },
+  {
+    "revision": "71e3705946188729e691",
+    "url": "/movie-app-project/static/js/1.71e37059.chunk.js"
+  },
+  {
+    "revision": "79fd940452eb42c66221",
+    "url": "/movie-app-project/static/css/main.cb61c4d2.chunk.css"
+  },
+  {
+    "revision": "4b5dd7161b877d8ab0db0a580fda9178",
+    "url": "/movie-app-project/index.html"
+  }
+];
